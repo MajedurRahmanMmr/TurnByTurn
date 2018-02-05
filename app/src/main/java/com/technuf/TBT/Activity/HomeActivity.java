c17103aec0a2ab0c38d580b2c7fb1a52b95dfdc4
Majedur Rahman <majedur.rahman.mmr@gmail.com>
2018-02-05 20:39:42 +0600

    initial commit

--- app/src/main/java/com/technuf/TBT/Activity/HomeActivity.java
+++ app/src/main/java/com/technuf/TBT/Activity/HomeActivity.java
@@ -5,19 +5,38 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.mapbox.geojson.Point;
 import com.technuf.TBT.R;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeActivity extends AppCompatActivity {
+    public static final String BUS_TOPS_LIST = "BusStopsList";
+    List<Point> points = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+        /** Todo
+         * Need to replace add Bus stops LatLng here
+         */
+
+        points.add(Point.fromLngLat(-77.2673379, 39.1751199));
+        points.add(Point.fromLngLat(-77.2677795, 39.1775459));
+        points.add(Point.fromLngLat(-77.2660765, 39.1776911));
+        points.add(Point.fromLngLat(-77.26183, 39.1831462));
+
+
+
         findViewById(R.id.start_nav).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HomeActivity.this, NavigationActivity.class));
+                startActivity(new Intent(HomeActivity.this, NavigationActivity.class).putExtra(BUS_TOPS_LIST, (Serializable) points));
             }
         });
     }
